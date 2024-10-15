5a8e006cb86952e8bdef904975519c27598ff0ce
jeantigreros <jtigreros92@gmail.com>
2024-10-15 16:02:54 -0500

    Add projects functionality

---
+++ src/const.js
@@ -0,0 +1,14 @@
+export const PROJECTS = [
+  {
+    slug: "url-shortener",
+    title: "URL Shortener (tinyURL clone)",
+    hidden: false,
+    description:
+      "This is a tiny url clone!",
+    tech: [
+      "example",
+      "example2",
+      "example3",
+    ]
+  },
+]
