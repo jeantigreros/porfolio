8cff735ab099b44ae5b02e3815950b76ac178346
Jean Tigreros <jtigreros92@gmail.com>
2026-03-15 18:55:20 -0500

    sinnsa

--- src/const.js
+++ src/const.js
@@ -30,4 +30,21 @@ export const PROJECTS = [
     demo: false,
     code: true,
   },
+  {
+    slug: "gruposinnsa",
+    title: "Grupo SiNNSA",
+    hidden: false,
+    description:
+    "Desarrollé una plataforma web full-stack para Grupo SINNSA orientada a fortalecer su presencia digital y facilitar el contacto con clientes. El proyecto incluyó el diseño de la interfaz, arquitectura del sistema y desarrollo del backend para gestionar contenido y consultas. La solución está optimizada para rendimiento, SEO y experiencia de usuario, permitiendo a la empresa presentar sus servicios de forma profesional y escalable.",
+    tech: [
+      "Frontend Development",
+      "Backend Development",
+      "Web Design",
+      "Systems Architecture",
+      "SEO Optimization"
+    ],
+    link: "https://gruposinnsa.com/",
+    demo: false,
+    code: false,
+  }
 ]
